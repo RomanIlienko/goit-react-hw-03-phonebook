2f544b413106c05583368330640afb35becb8b61
RomanIlienko <75617889+RomanIlienko@users.noreply.github.com>
2021-11-10 15:28:05 +0200

    fixed2

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ class App extends Component {
     };
     const { contacts } = this.state;
     const alreadyInContacts = contacts.find(
-      contact => contact.name === todo.name,
+      contact => contact.name === todo.name && contact.number === todo.number,
     );
     if (alreadyInContacts) {
       alert(`${alreadyInContacts.name} is already in contacts.`);
